433bfcf4e44215224a63bd6e7dd885784ef09dcf
bore-prog <philippeaurore@orange.fr>
2024-01-26 13:53:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from typing import Union
-
+from sklearn.datasets import fetch_openml
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/predict")
 def read_root(data=""):
     print(data)
+    X_adult, y_adult = fetch_openml("adult", version=2, return_X_y=True)
+
+    # Remove redundant and non-feature columns
+    X_adult = X_adult.drop(["education-num", "fnlwgt"], axis="columns")
+    X_adult.dtypes
     
     # myIA.predict
-    return{"Result": "Prediction"}
+    return X_adult
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
